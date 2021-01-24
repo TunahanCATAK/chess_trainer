bb6ae71889a97890a393d213996acd9d5d4175f0
Tunahan Catak <tcatak@Tunahans-MacBook-Pro.local>
2021-01-24 11:03:35 +0100

    class based views

--- helloFunction/views.py
+++ helloFunction/views.py
@@ -1,5 +1,9 @@
 # helloFunction/views.py
 from django.http import HttpResponse
+from django.views import View 
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+
+class HomePageView(View):
+    def get(self, request):
+        return HttpResponse('Hello, World!')
+
